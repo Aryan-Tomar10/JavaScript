1c62f6c67f6cb028d7c746b1dd02fd335ff0fe0d
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-16 12:21:04 +0530

    DataType Summary

---
+++ 01_basic/05_datatype-summery.js
@@ -0,0 +1,30 @@
+// Primitive data types
+
+//7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.1
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const bigNumber = 9007199254740991n
+
+console.log(typeof score);
+
+// Reference data types (Non-primitive data types)
+
+// Object, Array, Function, Date, RegExp, etc.
+
+
+const heros = ["Shaktiman", "Bheem", "Chhota Bheem", "Doremon"]
+let myobject = {
+    name: "Shaktiman", 
+    power: 100
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
