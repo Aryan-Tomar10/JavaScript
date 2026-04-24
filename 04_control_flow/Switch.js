56991542baf1d79e2477410d4c45dd663c4c8f35
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 11:02:21 +0530

    Use of Switch Case Statement and jump Operators

---
+++ 04_control_flow/Switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = "march"
+// switch (month) {
+//     case "January":
+//         console.log("January");
+//         break;
+//     case "February":
+//         console.log("February");
+//         break;
+//     case "march":
+//         console.log("March");
+//         break;
+//     case "April":
+//         console.log("April");
+//         break;
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
