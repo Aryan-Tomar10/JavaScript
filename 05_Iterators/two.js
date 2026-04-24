a045973dfb2d5f49a9f6d988788e8192f1792732
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 13:05:00 +0530

    Use of while and do while loop with array and normal numeric value

---
+++ 05_Iterators/two.js
@@ -0,0 +1,26 @@
+//while loop to print normal numaric value
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index +2
+// }
+
+
+//while loop with Array
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0;
+// while (arr<myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+//Do-While loop
+
+let score =11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
