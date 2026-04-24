b4e4de5f23938b32ca12b00bed9f61a9aba02ead
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 10:36:17 +0530

    Use of Conditional Statements, AND OR operator

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+//if
+const isUserLoggedIn = true;
+const temperature = 41
+
+// if (isUserLoggedIn) {}
+    
+// if (temperature===41) {
+//     console.log("executed");
+// }
+// console.log("temp is > 50");
+
+// if (temperature<=41) {
+//     console.log("executed");
+// }else{
+//     console.log("temp is > 50");
+// }
+// if (2!=3) {
+//     console.log("executed");
+// }
+
+// console.log("Executed");
+// <,>,<=,>=,==,!=,===,!==
+
+const score = 200;
+// if (score > 100) {
+//     const power = "fly" //dont use var. Use only const and let operator. 
+//     console.log(`User power : ${power}`);
+// }
+// console.log(`User score : ${power}`);
+
+
+// shorthand Notation
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"), console.log("test2");//this should not be done as this is not professionally correct.
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance <750) {
+//     console.log("less than 750");
+// }else if (balance <1200) {
+//     console.log("less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+
+if (loggedInFromEmail|| loggedInFromGoogle) {
+    console.log("User Logged In");
+    
+}
