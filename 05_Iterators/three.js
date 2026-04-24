93dd4c760597ba51490e7cbf8794bc1de86badb4
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 13:23:38 +0530

    Use of map object and forof loop with array and object

---
+++ 05_Iterators/three.js
@@ -0,0 +1,46 @@
+//for of 
+
+// ["","",""]
+// [{},{},{}]
+
+
+// const arr = [1,2,3,4,5]
+
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+
+// const greeting = "Hello World!"
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+
+
+//Maps
+
+// const map = new Map()
+// map.set('IN', 'INDIA')
+// map.set('USA', 'United States of America')
+// map.set('Fr', 'France')
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key);    
+// }
+
+// const myObj = {
+//     'game1' : 'NFS',
+//     'game2' : "Spiderman"
+// }
+
+// for (const [key,value] of myObj) {
+//     console.log(key);
+    
+// }
+
+//summary: forof loop works with array and map not with object
+
+
