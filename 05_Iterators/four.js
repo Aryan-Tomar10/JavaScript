d15fceda316da331584d3c5cbb67516786049017
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 13:32:04 +0530

    Use of map object and forin loop with array and object

---
+++ 05_Iterators/four.js
@@ -0,0 +1,36 @@
+// const myObject ={
+//     js:"javascript",
+//     cpp:"C++",
+//     rb:"Ruby",
+//     py:"Python",
+//     swift:"Swift"
+// }
+
+
+//use of forin loop with object 
+// for (const key in myObject) {
+//     console.log(`${key} stands for ${myObject[key]}`);
+// }
+
+
+//use of forin loop wit array
+// const arr = [1,2,3,4,5,6]
+
+// for (const key in arr) {
+//     console.log(arr[key]);    
+// }
+
+
+
+//forin loop not possible in map as it is not iterable
+// const map = new Map()
+// map.set('IN', 'INDIA')
+// map.set('USA', 'United States of America')
+// map.set('Fr', 'France')
+
+
+// for (const [key,value] in map) {
+//     console.log(`${key} stands for ${value}`);
+// }
+
+
