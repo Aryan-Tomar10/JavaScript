60ad3c6ffddca794f1ecb592d59ce60d27fa8d1d
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-04-24 12:05:52 +0530

    Use of If-else statement, knowledge of falsy and truey values , use of Object in if-else statement and use of Terniary operator, Nullish Coalescing Operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,11 +1,11 @@
-// const userEmail = []
+const userEmail = []
 
-// if (userEmail) {
-//     console.log("got user email");   
-// }else{
-//     console.log("Don't have user Email");
+if (userEmail) {
+    console.log("got user email");   
+}else{
+    console.log("Don't have user Email");
     
-// }
+}
 
 // falsy values
 
@@ -14,32 +14,32 @@
 // Truely Value
 //"0", 'false', " ", [], {}, function(){},
 
-// if (userEmail.length === 0){
-//     console.log("Array is empty")
-// }
+if (userEmail.length === 0){
+    console.log("Array is empty")
+}
 
 
-// const emptyObject = {}
+const emptyObject = {}
 
-// if(Object.keys(emptyObject).length === 0){
-//     console.log("Object is empty");
+if(Object.keys(emptyObject).length === 0){ //Object.keys(emptyObject) this will return a array
+    console.log("Object is empty");
     
-// }
+}
 
 
-// console.log(false ==0);
-// console.log(false =="");
-// console.log(0 =="");
+console.log(false ==0);
+console.log(false =="");
+console.log(0 =="");
 
 
 //Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10;
-// val1 = null ?? 10;
-// val1 = undefined ?? 10;
-// val1 = null ?? 10 ?? 20;
-// console.log(val1);
+val1 = 5 ?? 10;
+val1 = null ?? 10;
+val1 = undefined ?? 10;
+val1 = null ?? 10 ?? 20;
+console.log(val1);
 
 
 
