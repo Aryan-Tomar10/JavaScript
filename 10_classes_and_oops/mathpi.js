3891928231ae7cf9f687d5391a0c372e9252aac3
Aryan-Tomar10 <aryantomar1005@gmail.com>
2026-05-01 16:39:13 +0530

    Knowledge of how to define properties of the object and get to know why we can't change the value of math.pi

---
+++ 10_classes_and_oops/mathpi.js
@@ -0,0 +1,41 @@
+// const descriptor = Object.getOwnPropertyDescriptor(Math,'PI')
+
+
+// // console.log(Math.PI);
+// // Math.PI = 5
+// // console.log(Math.PI);
+
+
+// console.log(descriptor);
+const chai  = {
+    name:'ginger chai',
+    price : 250,
+    isAvailable : true,
+
+    orderchai : function(){
+        console.log("chai nhi bni");
+        
+    }
+}
+
+// console.log(chai);
+
+// console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+Object.defineProperty(chai,'name',{
+    writable : false,
+    enumerable : true,
+    // configurable : false
+})
+// // console.log(Object.getOwnPropertyDescriptor(chai,'name'));
+
+
+// console.log(chai);
+
+
+for (const [key,value] of Object.entries(chai)) {
+    if (typeof value !== "function"){
+        console.log(`${key} : ${value}`);
+    }
+    
+}
